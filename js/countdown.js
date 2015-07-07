733dc267282c7b89c36391f020dcbf70b19a2bbc
root <root@raviwedsyadu.com>
2015-07-07 00:45:06 -0400

    Added UTC datetime

--- js/countdown.js
+++ js/countdown.js
@@ -1,5 +1,4 @@
-var weddingDateStr = "2016/02/17";
-var weddingDate = new Date(weddingDateStr);
+var weddingDate = new Date("2016/02/17 00:00:00 GMT+0530");
 
 var updateCountdownCallback = function(event) {
   $(this).html(event.strftime("%-D day%!D, %H hour%!H, %M minute%!M, %S second%!S"));
@@ -7,9 +6,9 @@ var updateCountdownCallback = function(event) {
 
 var finishedCountdownCallback = function(event) {
   $(".countdown-timer-text").hide();
-  $(this).html("Ravi and Yadu were married on " + weddingDate.toDateString());
+  $(this).html("Ravi and Yadu were married on " + weddingDate.toUTCString());
 };
 
-$(".countdown-timer").countdown(weddingDateStr)
+$(".countdown-timer").countdown(weddingDate)
   .on("update.countdown", updateCountdownCallback)
   .on("finish.countdown", finishedCountdownCallback);
